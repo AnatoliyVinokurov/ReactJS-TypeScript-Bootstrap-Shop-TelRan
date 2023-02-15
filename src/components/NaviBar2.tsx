8b34e9d3392614c5795b15a8ac9d373148064f5f
AnatoliyVinokurov <anatolij.isit@gmail.com>
2023-02-15 18:15:24 +0200

    finished nav bar

--- src/components/NaviBar2.tsx
+++ src/components/NaviBar2.tsx
@@ -11,40 +11,41 @@ export default function NaviBar() {
     return (
 
         <header>
-        <Navbar className='navbar' expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="#" className='logo'>LOGO</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link className='navelementbtn' href="#action1">Men</Nav.Link>
-                        <Nav.Link className='navelementbtn' href="#action2">Women</Nav.Link>
-                        <Nav.Link className='navelementbtn' href="#action3">Kids</Nav.Link>
-                        <Nav.Link className='navelementbtn' href="#action4">Sale</Nav.Link>
-                        <Nav.Link className='navelementbtn' href="#action5">Collections</Nav.Link>
-                        <Nav.Link className='navelementbtn' href="#action6">Blog</Nav.Link>
-                    </Nav>
+            <Navbar className='navbar' bg='dark' variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#" className='logo'>LOGO</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link className='navelementbtn' href="#action1">Men</Nav.Link>
+                            <Nav.Link className='navelementbtn' href="#action2">Women</Nav.Link>
+                            <Nav.Link className='navelementbtn' href="#action3">Kids</Nav.Link>
+                            <Nav.Link className='navelementbtn' href="#action4">Sale</Nav.Link>
+                            <Nav.Link className='navelementbtn' href="#action5">Collections</Nav.Link>
+                            <Nav.Link className='navelementbtn' href="#action6">Blog</Nav.Link>
+                        </Nav>
 
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        
-                        <Button variant="outline-success">Search</Button>
-                        <Button variant="outline-success">Search</Button>
-                        <Button variant="outline-success">Search</Button>
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                        <Nav className="ms-auto">
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                            </Form>
+
+                            <Nav.Link className='navelementbtn' href="#action7">
+                                <span className="badge badge-pill bg-danger">1</span>
+                                <span><i className="fas fa-shopping-bag"></i></span>
+                            </Nav.Link>
+                            <Nav.Link className='navelementbtn' href="#action8"><i className="fas fa-user-alt"></i></Nav.Link>
+
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
 
     )
